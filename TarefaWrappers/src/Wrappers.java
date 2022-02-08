14325b5d6df28f29423552e683ec4fbf206bc0ef
Rai <78910563+Rai-Git@users.noreply.github.com>
2022-02-08 12:26:57 -0300

    Exercício Wrappers
    Exercício Wrappers.

---
+++ TarefaWrappers/src/Wrappers.java
@@ -0,0 +1,17 @@
+public class Wrappers
+{
+    public static void main(String args[])
+    {
+        int cpf = 125896;
+        System.out.println(cpf);
+        Integer cpf1 = cpf;
+        System.out.println(cpf1);
+        System.out.println();
+
+        int caixamenor = 1256863322;
+        Long caixamaior = (long) caixamenor;
+        System.out.println(caixamenor);
+        System.out.println(caixamaior);
+
+    }
+}
