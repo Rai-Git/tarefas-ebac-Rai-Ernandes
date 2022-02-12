83c38bc456adc2c5908a3ebbb7ee20a4a2d41d00
Rai <78910563+Rai-Git@users.noreply.github.com>
2022-02-12 18:29:07 -0300

    Tarefa Wrappers refeita
    Tarefa Wrappers refeita.

---
+++ TarefaWrappers/src/Cast.java
@@ -0,0 +1,69 @@
+public class Cast
+{
+
+    private int primeiro;
+    private char segundo;
+    private short terceiro;
+    private byte quarto;
+    private long quinto;
+    private float sexto;
+    private double setimo;
+
+    public int getPrimeiro()
+    {
+        return primeiro;
+    }
+    public void setPrimeiro(int primeiro)
+    {
+        this.primeiro = primeiro;
+    }
+    /*public char getSegundo()
+    {
+        return segundo;
+    }
+    public void setSegundo(char segundo)
+    {
+        this.segundo = segundo;
+    }*/
+    /*public short getTerceiro()
+    {
+        return terceiro;
+    }
+    public void setTerceiro(short terceiro)
+    {
+        this.terceiro = terceiro;
+    }*/
+   /* public byte getQuarto()
+    {
+        return quarto;
+    }
+    public void setQuarto(byte quarto)
+    {
+        this.quarto = quarto;
+    }*/
+    public long getQuinto()
+    {
+        return quinto;
+    }
+    public void setQuinto(long quinto)
+    {
+        this.quinto = quinto;
+    }
+    public float getSexto()
+    {
+        return sexto;
+    }
+    public void setSexto(float sexto)
+    {
+        this.sexto = sexto;
+    }
+    public double getSetimo()
+    {
+        return setimo;
+    }
+    public void setSetimo(double setimo)
+    {
+        this.setimo = setimo;
+    }
+
+}
--- TarefaWrappers/src/Wrappers.java
+++ TarefaWrappers/src/Wrappers.java
@@ -1,17 +1,57 @@
-public class Wrappers
-{
-    public static void main(String args[])
+
+    public class Wrappers
     {
-        int cpf = 125896;
-        System.out.println(cpf);
-        Integer cpf1 = cpf;
-        System.out.println(cpf1);
-        System.out.println();
 
-        int caixamenor = 1256863322;
-        Long caixamaior = (long) caixamenor;
-        System.out.println(caixamenor);
-        System.out.println(caixamaior);
+        public static void main(String args[])
+        {
+            int a;
+            long b;
+            float c;
+            double d;
 
+            //Primeiro é (int)
+            //Segundo é (char)
+            //Terceiro é (short)
+            //Quarto é (byte)
+            //Quinto é (long)
+            //Sexto é (float)
+            //Sétimo é (double)
+
+            Cast cast = new Cast();
+            cast.setPrimeiro(1111111111);
+            System.out.println("int  " + cast.getPrimeiro());
+            //cast.setSegundo(1);
+            //System.out.println("char  " + cast.getSegundo());
+            //cast.setTerceiro(1);
+            //System.out.println("short  " + cast.getTerceiro());
+            //cast.setQuarto(1);
+            //System.out.println("byte  " + cast.getQuarto());
+            cast.setQuinto(1111111111111111111l);
+            System.out.println("long  " + cast.getQuinto());
+            cast.setSexto(2.2222222f);
+            System.out.println("float  " + cast.getSexto());
+            cast.setSetimo(2.222222222222222);
+            System.out.println("double  " + cast.getSetimo());
+
+            System.out.println("******************");
+            System.out.println("Explícito");
+            //Casting Explícito.
+            a = (int) cast.getQuinto();
+            System.out.println("De long para int   " + a);
+            c = (float) cast.getSetimo();
+            System.out.println("double para float   " + c);
+            b = (long) cast.getSetimo();
+            System.out.println("double para long   " + b);
+
+            System.out.println("******************");
+            System.out.println("Implícito");
+            //Casting Implícito.
+            c = cast.getPrimeiro();
+            System.out.println("int para float   " + c);
+            b = cast.getPrimeiro();
+            System.out.println("int para long   " + b);
+            d = cast.getSexto();
+            System.out.println("float para double   " + d);
+
+        }
     }
-}
